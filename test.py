975abfcb4564b259a60451eec3e281a686b4c9dd
fran <fran@frans-MacBook-Pro.local>
2023-11-23 22:52:03 -0300

    add basic grid and test

---
+++ test.py
@@ -0,0 +1,30 @@
+import unittest
+from io import StringIO
+import sys
+from buscaminas import Cell, BuscaMinas
+
+class TestCell(unittest.TestCase):
+    def test_cell_initial_state(self):
+        cell = Cell(0)
+        self.assertTrue(cell.hidden)
+        self.assertEqual(cell.content, 0)
+
+class TestBuscaMinas(unittest.TestCase):
+    def test_grid_initialization(self):
+        game = BuscaMinas(5, 4)
+        self.assertEqual(len(game.grid), 4)
+        for row in game.grid:
+            self.assertEqual(len(row), 5)
+
+    def test_display_output(self):
+        game = BuscaMinas(2, 2)
+        expected_output = "\n. . \n. . \n\n"
+
+        original_stdout = sys.stdout  # Save a reference to the original standard output
+        sys.stdout = StringIO()       # Redirect standard output to a string
+        game.display()
+        self.assertEqual(sys.stdout.getvalue(), expected_output)
+        sys.stdout = original_stdout  # Reset the standard output to its original value
+
+if __name__ == '__main__':
+    unittest.main()
